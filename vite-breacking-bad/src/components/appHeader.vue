b215701a443638e7ae54b5b7b639daadc3ae8637
nembo93 <francesco.c193@libero.it>
2022-11-28 16:15:38 +0100

    Aggiunge component appHeader a App.vue

--- vite-breacking-bad/src/components/appHeader.vue
+++ vite-breacking-bad/src/components/appHeader.vue
@@ -1,15 +1,23 @@
 <script>
-import appHeader from "./appHeader.vue";
-
 export default {
     name: "appHeader",
 };
 </script>
 
 <template>
-    <img src="../assets/breacking-bad.png" alt="logo">
-    <h1>Breacking Bad Api</h1>
+    <header>
+        <img class="header_logo" src="../assets/breacking-bad.png" alt="logo">
+        <h1 class="header_title">Breacking Bad Api</h1>
+    </header>
 </template>
 
-<style>
+<style scoped>
+.header_logo{
+    width: 200px;
+    height: 200px;
+}
+.header_title{
+    color: red;
+}
+
 </style>
