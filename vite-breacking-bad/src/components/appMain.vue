7bff1ac1db184efda31c0d100907d06a553ac206
nembo93 <francesco.c193@libero.it>
2022-11-28 16:56:56 +0100

    Aggiunge main

---
+++ vite-breacking-bad/src/components/appMain.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: "appMain",
+};
+</script>
+
+<template>
+    <div class="container">
+        <div class="main_top">
+            <select id="">
+                <option value="">Select category</option>
+            </select>
+        </div>
+
+        <div class="main_box">
+
+        </div>
+    </div>
+</template>
+
+<style>
+.container{
+    width: 800px;
+    background-color: yellow;
+    margin: auto;
+}
+.main_top{
+    background-color: azure;
+    padding: 20px;
+}
+.main_box{
+    background-color: pink;
+    height: 400px;
+}
+</style>
